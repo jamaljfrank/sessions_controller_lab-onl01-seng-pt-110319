c600ed03e21a9e28ec4904dca1e565c4165685b5
Jamal Frank <jamaljfrank@gmail.com>
2020-04-28 06:45:04 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+redirect_to login_pathRails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
   get '/login' => 'sessions#new'
